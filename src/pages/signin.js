48350249abc16bed6b706e0590df32233fe1e546
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2020-11-29 12:49:21 -0300

    criação form e style para tela login e password

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,34 @@
-import { func } from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
+import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
+import { Form } from '../components';
 
 export default function Signin() {
-  return <p>Hello from the sign up!</p>
-  
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPasswrod] = useState();
+  const [error, setError] = useState(' ');
+
+
+  // check form input elements are valid
+  // email & password 
+
+  return <>
+  <HeaderContainer>
+    <Form>
+        <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)} 
+              /> 
+
+          </form.Base>
+          </Form>
+  </HeaderContainer>
+  <FooterContainer />
+  </>
+  );
 }
