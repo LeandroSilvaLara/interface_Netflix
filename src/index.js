4afc83c006388cb95639a0bcfad78d69b8805b03
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2020-11-29 11:38:55 -0300

    Firebase

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import App from './app';
 import 'normalize.css';
 import { render } from 'react-dom';
 import { GlobalStyles } from './global-styles';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext} from './context/firebase';
 
 render(<>
+  <FirebaseContext.Provider value={{ firebase }}>
   <GlobalStyles />
   <App />
+  </FirebaseContext.Provider>
   </>,
-  document.getElementById('root'));  
+  document.getElementById('root')
+  
+  );  
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,8 +6,19 @@ import 'firebase/auth';
 
 // we need a config here
 
-const config = {};
+const config = {
+    apiKey: "AIzaSyAcEtMW9Z48pcZb3Ryvm5lwQ0rkQL63GoM",
+    authDomain: "netflix-project-82e36.firebaseapp.com",
+    databaseURL: "https://netflix-project-82e36.firebaseio.com",
+    projectId: "netflix-project-82e36",
+    storageBucket: "netflix-project-82e36.appspot.com",
+    messagingSenderId: "238794647813",
+    appId: "1:238794647813:web:a4d95c4be3e667f47cef6c",
+
+};
 
 const firebase = Firebase.initializeApp(config);
+
+
 
 export { firebase };
