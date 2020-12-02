d6811fa03a1b8695a153131cd24517085e7baae7
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2020-12-01 21:55:43 -0300

    signin e signup crianção de tela de cadastro

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,51 @@
-import { func } from 'prop-types';
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function Signup() {
-  return <p>Hello from the sign up!</p>
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+
+    // do firebase stuff
+    
+  };
+
+  return (
+  
+  <HeaderContainer>
+    <Form>
+      <Form.Title>Sign Up</Form.Title>
+  {error && <Form.Error>{error}</Form.Error>}
+
+  <Form.Base onSubmit={handleSignup} method="POST">
+    <Form.Input
+        placeholder="First name"
+        value={firstName}
+        onChange={({ target }) => setFirstName(target.value)}
+        />
+        <Form.Input
+            placeholder="Email address"
+            value={emailAddress}
+            onChance={({ target }) => setEmailAddress(target.value)}
+            />
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+  );
   
 }
